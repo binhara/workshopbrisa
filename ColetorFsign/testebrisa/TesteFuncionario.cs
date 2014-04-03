aa88c73a22baaa05913deb29609f79bb790e05d9
AlexsandroMarquesValle <aleks.skater@hotmail.com>
2014-04-03 20:24:35 -0300

    finalizado funcionario

---
+++ ColetorFsign/testebrisa/TesteFuncionario.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using libbrisa;
+
+namespace testebrisa
+{
+	[TestFixture ()]
+	public class TesteFuncionario
+	{
+		[Test ()]
+		public void ListarFuncionario ()
+		{
+			Funcionario fun = new Funcionario ();
+			Assert.IsNotNull (fun);
+			Assert.AreEqual (fun.ListarFuncionario ().Count , 3);
+
+		}
+		[Test]
+		public void ListarFuncionarioPorId ()
+		{
+
+			var fun = Funcionario.ListarFuncionarioPorId(2);
+
+			Assert.IsNotNull (fun);
+			Assert.AreEqual (fun.Id, 2);
+
+		}
+	}
+}
+
